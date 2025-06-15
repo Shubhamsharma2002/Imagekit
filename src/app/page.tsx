dba56655856ffdd1bc43c8720c4025be16ef71b1
shubham sharma <shubhamtech004@gmail.com>
2025-06-15 23:00:35 +0530

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-3xl text-red-300">working on it image kit app </div>
+    <div className="text-3xl text-red-300">working on it image kit app
+    
+      <br />
+      <p>due to some isuue we are delaying this</p>
+     </div>
   );
 }

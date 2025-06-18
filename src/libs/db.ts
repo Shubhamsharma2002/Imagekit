9f69057755f6b9439dd811bbe7e936ee1ccfacb3
shubham sharma <shubhamtech004@gmail.com>
2025-06-19 02:34:36 +0530

    db connection code done

---
+++ src/libs/db.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+if (!MONGODB_URI) {
+  throw new Error("plese define mongouri in env varialbe");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDb() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    mongoose.connect(MONGODB_URI).then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+}

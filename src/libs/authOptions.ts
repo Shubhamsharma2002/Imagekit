a130d43aeae1ee85da429f843fd1aa9f44ea3d39
shubham sharma <shubhamtech004@gmail.com>
2025-06-21 23:54:30 +0530

    auth options providers

---
+++ src/libs/authOptions.ts
@@ -0,0 +1,49 @@
+import { NextAuthOptions } from "next-auth";
+
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDb } from "./db";
+import bcrypt from "bcryptjs";
+import User from "@/models/User";
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "passsword" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing email or passsword");
+        }
+
+        try {
+          await connectToDb();
+          const user = await User.findOne({ email: credentials.email });
+
+          if (!user) {
+            throw new Error("No user found with this");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.passsword
+          );
+
+          if (!isValid) {
+            throw new Error("invalid password");
+          }
+
+          return {
+            id: user._id.toString(),
+            email: user.email,
+          };
+        } catch (error) {
+          console.error("Auth error: ", error);
+          throw error;
+        }
+      },
+    }),
+  ],
+  callbacks: {},
+};

e1beef8dcbe57ac7696ea3d9fb35defbb8b4f496
shubham sharma <shubhamtech004@gmail.com>
2025-06-24 18:51:30 +0530

    login page api section done

---
+++ src/app/login/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+
+const LoginPage = () => {
+      const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (result?.error) {
+      console.log(result.error);
+    } else {
+      router.push("/");
+    }
+  };
+
+  return (
+    <div>LoginPage</div>
+  )
+}
+
+export default LoginPage
